07098add57b5fb8142d6ba59c9478bd4da23c34a
Steve Shilling <stevshil@users.noreply.github.com>
2020-05-29 16:28:44 +0100

    Openshift 3.11 working

--- kubernetes/bin/minikube.sh
+++ kubernetes/bin/minikube.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
-  && chmod +x minikube
+# Install Docker requirement
+yum -y install docker || (echo "Failed to install Docker" && exit 1)
 
-mv minikube /usr/local/bin
+# Enable and start docker
+systemctl enable docker
+systemctl start docker
 
-minikube start
+# Turn off swap and permanently
+swapoff -a
+sed -i '/swap/d' /etc/fstab
+
+# Set bridge-nf-call-iptables to 1
+sysctl -w net.bridge.bridge-nf-call-iptables=1
+
+# Download minikube and install
+if [[ ! -f /usr/local/bin/minikube ]]
+then
+  if curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+  then
+    chmod +x minikube
+    mv minikube /usr/local/bin
+  fi
+fi
+
+# Start Kubernetes
+if [[ -f /usr/local/bin/minikube ]]
+then
+  /usr/local/bin/minikube --vm-driver=none start
+  #/usr/local/bin/minikube --vm-driver=virtualbox start
+  #/usr/local/bin/minikube config set vm-driver virtualbox
+fi
