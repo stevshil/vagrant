a5d8e85317fdefb7278132e2c8241fc48059e064
Steve Shilling <steve.shilling@automationlogic.com>
2023-03-24 15:32:38 +0000

    updates

--- neueda/bin/add-cd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Script to insert the guest additions ISO from CLI
-
-#VBoxManage storageattach "$1" --storagectl "IDE Controller" --port 0 --device 1 --type dvddrive --medium "/usr/share/virtualbox/VBoxGuestAdditions.iso"
-VBoxManage storageattach "$1" --storagectl "IDE Controller" --port 0 --device 1 --type dvddrive --medium additions
